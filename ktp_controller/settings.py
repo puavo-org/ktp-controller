df44055ba34986cd3c47d9245e033bfadd28e94e
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2025-09-03 21:58:17 +0300

    Add settings for exam-o-matic connections

--- ktp_controller/settings.py
+++ ktp_controller/settings.py
@@ -3,6 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     exam_o_matic_host: str
+    exam_o_matic_username: str
+    exam_o_matic_password_file: str
+    domain: str
+    hostname: str
+    id: str
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", env_prefix="KTP_CONTROLLER_"
