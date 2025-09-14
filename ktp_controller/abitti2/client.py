246ddc734c0f550096438f066f92235704f97d28
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2025-09-14 20:22:58 +0300

    Add abitti2 client lib

---
+++ ktp_controller/abitti2/client.py
@@ -0,0 +1,28 @@
+import typing
+
+import requests
+import requests.auth
+
+import ktp_controller.utils
+import ktp_controller.abitti2.utils
+
+
+def get_json(path: str, *, timeout: int = 20) -> typing.Any:
+    host = ktp_controller.abitti2.utils.read_domain()
+    url = ktp_controller.utils.get_url(host, path)
+
+    response = requests.get(
+        url,
+        auth=requests.auth.HTTPBasicAuth(
+            "valvoja", ktp_controller.abitti2.utils.read_password()
+        ),
+        timeout=timeout,
+    )
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+def get_version() -> str:
+    return get_json("/api/version")["version"]
