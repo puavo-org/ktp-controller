397d7eca40cbd63b76e45eceac60ab2f52bb551d
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2025-11-24 22:57:36 +0200

    ktp_controller.examomatic.mock.data: add Integraatiotestikoe1

--- ktp_controller/examomatic/mock/data.py
+++ ktp_controller/examomatic/mock/data.py
@@ -105,7 +105,15 @@ _EXAM_FILE_INFOS = {
         "file_sha256": "fd3a4a6021d4b53daa4494db4b907085917ab542ee10864d426abdf01d5ccc8f",
         "file_uuid": "12cb630c-9419-4fa8-93f7-f1b01779e470",
         "decrypt_code": "kukka ahma reiluus sotaonni",
-    }
+    },
+    "Integraatiotestikoe1": {
+        "exam_title": "Integraatiotestikoe1",
+        "file_name": "exam_Integraatiotestikoe1.mex",
+        "file_size": 25353142,
+        "file_sha256": "50d28d5ce4628d9e72c3d42001a49f9fbc146081fbac42610435d6c70d4f6624",
+        "file_uuid": "c574f93a-ac4d-4441-8679-ca47e565fb7b",  # UUID from oma.abitti.fi/school/exams
+        "decrypt_code": "itarasti toutain edustava myllytys",
+    },
 }
 
 
