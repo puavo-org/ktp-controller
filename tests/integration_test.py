af29b4dba5947ffe001fdd86112d7ccbd4a5089c
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2025-11-27 21:01:06 +0200

    Update test_single_exam_package plan

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -13,19 +13,21 @@ from ktp_controller.settings import SETTINGS
 def test_single_exam_package():
     utcnow = datetime.datetime.utcnow()
 
+    # 0. spawn all services (examomatic-mock, api, agent)
     # 1. wait until agent has called home for the first time
-    # 1. check status report which tells us if Abitti2 is ok (dummy exam running)
-    # 1. prime examomatic-mock with exam info
-    # 1. wait until agent has called home
-    # 1. check status report which tells us if Abitti2 is ok (integration test exam ready)
-    # 1. wait until agent has called home
-    # 1. check status report which tells us if Abitti2 is ok (integration test exam running)
-    # 1. wait until agent has called home
-    # 1. check status report which tells us if Abitti2 is ok (integration test exam stopping)
-    # 1. wait until agent has called home
-    # 1. check status report which tells us if Abitti2 is ok (integration test exam stopped)
-    # 1. wait until agent has called home
-    # 1. check status report which tells us if Abitti2 is ok (integration test exam archived)
+    # 2. check status report which tells us if Abitti2 is ok (dummy exam running)
+    # 3. prime examomatic-mock with exam info (single scheduled exam, time intervals are short for testing purposes: 30sec pre-lock time, 30sec lock time, 30 sec run time)
+    # 4. wait until agent has refreshed exams (should happen almost instantly since examomatic-mock sends refresh message via websocket)
+    # 5. check status report which tells us if Abitti2 is ok (integration test exam ready)
+    # 6. wait until start time
+    # 7. check status report which tells us if Abitti2 is ok (integration test exam running)
+    # 8. wait until end time
+    # 9. check status report which tells us if Abitti2 is ok (integration test exam stopping)
+    # 10. wait until exam is stopped completely
+    # 11. check status report which tells us if Abitti2 is ok (integration test exam stopped)
+    # 12. wait
+    # 13. check status report which tells us if Abitti2 is ok (integration test exam archived)
+    # 14. are exam answers still uploaded when there were no students? (student automation not included)
 
     agent_has_called_home = False
     for i in range(15):  # First ping-pong round should not take more than 15sec
