bb6255f330abb00473d62a0ada3817a9b151e755
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2025-12-09 18:18:56 +0200

    Remove cruft from aux script

--- aux/setup-puavo-localhost.sh
+++ aux/setup-puavo-localhost.sh
@@ -30,16 +30,11 @@ fi
 
 sudo rsync -r --delete-after "$(git rev-parse --show-toplevel)/" /home/puavo-ers/ktp-controller/
 sudo chown -R puavo-ers:puavo-ers /home/puavo-ers/ktp-controller
-#sudo install -m0640 -oroot -groot /dev/null /etc/sudoers.d/ktp-controller
-#sudo sh -c "echo '%puavo-ers      ALL = (:puavo) NOPASSWD: /usr/bin/make dev-run' >>/etc/sudoers.d/ktp-controller"
-head -n1 /etc/resolv.conf | grep -n -x 'nameserver 9.9.9.9' /etc/resolv.conf || {
-  sudo sed -r -i '1 i nameserver 9.9.9.9' /etc/resolv.conf
-}
 
 for g in puavo; do
     groups puavo-ers | tr ' ' '\n' | grep -x "$g" || {
-        terminate_session=true
         sudo adduser puavo-ers "$g"
+        terminate_session=true
     }
 done
 
