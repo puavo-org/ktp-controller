bdf57890d81b1c3032ce6c9dd2c8e82e4e41cbe9
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2026-02-13 18:05:40 +0200

    Fix formatting
    (cherry picked from commit 3907d2653e57499836d764cc44ad0202b44579c0)

--- ktp_controller/agent/main.py
+++ ktp_controller/agent/main.py
@@ -658,7 +658,7 @@ class Agent:
         }
 
         state = current_exam_package["state"]
-        if state == 'running':
+        if state == "running":
             self.__ensure_answer_transfer_task_is_running(current_exam_package)
 
         transition = state_transitions[state]
