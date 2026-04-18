c986fc2c4862207da5ee08ee86bc87949b8096eb
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2026-04-18 16:48:55 +0300

    Import puavo-ers-abitti2server and puavo-ers-abitti2server-networking

---
+++ ktp_controller/abitti2/naksu2monitor/__main__.py
@@ -0,0 +1,8 @@
+# Standard library imports
+import sys
+
+# Internal imports
+import ktp_controller.abitti2.naksu2monitor.main
+
+if __name__ == "__main__":
+    sys.exit(ktp_controller.abitti2.naksu2monitor.main.run())
---
+++ ktp_controller/networking/__main__.py
@@ -0,0 +1,8 @@
+# Standard library imports
+import sys
+
+# Internal imports
+import ktp_controller.networking.main
+
+if __name__ == "__main__":
+    sys.exit(ktp_controller.networking.main.run())
