b599064e76caf59a37fe9f6c9cd7340f40cc2c0c
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2026-04-18 14:38:09 +0300

    ktp_controller.utils: add logging_singleton_app()

--- ktp_controller/utils.py
+++ ktp_controller/utils.py
@@ -359,3 +359,42 @@ def ago(dt: str | datetime.datetime, *, now: datetime.datetime | None = None) ->
 
 def is_puavo_os() -> bool:
     return os.path.exists("/etc/puavo")
+
+
+def logging_singleton_app(
+    main_func,
+    logger,
+    stderr_logging_level=logging.ERROR,
+    stdout_logging_level=logging.WARNING,
+):
+    original_stderr = sys.stderr
+
+    if stderr_logging_level is not None:
+        sys.stderr = LineBufferedLoggingStream(logger, stderr_logging_level)
+    if stdout_logging_level is not None:
+        sys.stdout = LineBufferedLoggingStream(logger, stdout_logging_level)
+
+    logging_handlers = [
+        logging.handlers.SysLogHandler(address="/dev/log"),
+        logging.StreamHandler(original_stderr),
+    ]
+
+    logging.basicConfig(
+        level=logging.INFO,
+        handlers=logging_handlers,
+        force=True,
+    )
+    try:
+        logger.info("acquiring singleton program lock")
+        with singleton():
+            logger.info("calling main function")
+            status = main_func()
+        logger.log(
+            logging.INFO if status == 0 else logging.ERROR,
+            "returned from main function, status %d",
+            status,
+        )
+    except Exception:
+        logger.exception("failed")
+        raise
+    sys.exit(status)
