acdb767d76ff8626a82af9f102515b5a4fd53b59
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2026-04-28 21:17:58 +0300

    agent: do not include duplicate (based on sha256) exam files in the exam package

--- ktp_controller/agent/exam_package.py
+++ ktp_controller/agent/exam_package.py
@@ -59,21 +59,36 @@ async def create_exam_package_file(
 
     Returns a tuple of (exam_package_filepath, decrypt_codes).
     """
-    exam_file_infos = []
+
+    _LOGGER.info(
+        "creating exam package file for scheduled exam package %r...",
+        api_scheduled_exam_package["external_id"],
+    )
+
+    exam_file_infos = {}
     for api_scheduled_exam_external_id in api_scheduled_exam_package[
         "scheduled_exam_external_ids"
     ]:
         api_scheduled_exam = await ktp_controller.api.client.get_scheduled_exam(
             api_scheduled_exam_external_id
         )
-        exam_file_infos.append(api_scheduled_exam["exam_file_info"])
+        exam_file_info = api_scheduled_exam["exam_file_info"]
+        exam_file_sha256 = exam_file_info["sha256"].lower()
+        if exam_file_sha256 in exam_file_infos:
+            _LOGGER.warning(
+                "Duplicate exam file detected! Exam file with SHA256 %r is already in the package, skipping %r",
+                exam_file_sha256,
+                exam_file_info,
+            )
+            continue
+        exam_file_infos[exam_file_sha256] = exam_file_info
 
     decrypt_codes: typing.Set[str] = set()
     exam_package_filepath = ktp_controller.files.get_local_filepath(
         ktp_controller.files.LocalFilepathType.EXAM_PACKAGE,
         api_scheduled_exam_package["external_id"],
         hashlib.sha256(
-            "".join(sorted([i["sha256"] for i in exam_file_infos])).encode("ascii")
+            "".join(sorted([sha256 for sha256 in exam_file_infos])).encode("ascii")
         ).hexdigest(),
     )
 
@@ -81,7 +96,7 @@ async def create_exam_package_file(
         exam_package_filepath
     ) as exam_package_file:
         with zipfile.ZipFile(exam_package_file, "w") as exam_package_file_zip:
-            for exam_file_info in exam_file_infos:
+            for exam_file_info in exam_file_infos.values():
                 exam_package_file_zip.write(
                     ktp_controller.files.get_local_filepath(
                         ktp_controller.files.LocalFilepathType.EXAM_FILE,
@@ -91,6 +106,12 @@ async def create_exam_package_file(
                     ktp_controller.utils.utcnow_str() + exam_file_info["name"],
                 )
                 decrypt_codes.add(exam_file_info["decrypt_code"])
+
+    _LOGGER.info(
+        "created exam package file %r with %d exams",
+        exam_package_filepath,
+        len(exam_file_infos),
+    )
 
     return exam_package_filepath, decrypt_codes
 
