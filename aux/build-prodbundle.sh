e9ee7ed3efce811bdc17dea33a08c23802766847
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2026-05-12 18:44:06 +0300

    Fix Python version of build environment to 3.11.1 and revert the bundle filename back what it was
    (cherry picked from commit f573c8c6d10b8e0b1ee5f15b86b862c66309baae)

--- aux/build-prodbundle.sh
+++ aux/build-prodbundle.sh
@@ -31,9 +31,7 @@ uv export --no-emit-project --no-dev --format requirements-txt >dist/requirement
 
 echo "🚚 Collecting packages into bundle..." >&2
 version=$(uv version --short)
-pyversion=$(echo -n "${UV_PYTHON}" | tr '.' '_')
-bundle_name="ktp_controller-prodbundle-py${pyversion}-${version}"
-dir="dist/${bundle_name}"
+dir="dist/ktp_controller-prodbundle-${version}"
 rm -rf "$dir"
 mkdir -p "$dir"
 uv pip install \
@@ -56,8 +54,8 @@ find "$dir/ktp-controller/bin" -type f -exec sed -i '1 s|^#!.*\(python[0-9]*\).*
 
 echo "🗜️ Packing the bundle..." >&2
 cd "$dir"
-zip --symlinks --quiet -r "../${bundle_name}.zip" ktp-controller
+zip --symlinks --quiet -r "../ktp_controller-prodbundle-${version}.zip" ktp-controller
 
-echo "✅ Successfully created 'dist/${bundle_name}.zip'" >&2
+echo "✅ Successfully created 'dist/ktp_controller-prodbundle-${version}.zip'" >&2
 
 exitval=0
