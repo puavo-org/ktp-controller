15218fec79c2b720cfd0d7180d592402d59c3024
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2026-05-25 23:12:57 +0300

    agent: ensure answers are always uploaded in ascending timestamp order
    (cherry picked from commit 6b8c581bf1f49156dcfceb39a4090fddf3020b36)

--- ktp_controller/agent/main.py
+++ ktp_controller/agent/main.py
@@ -1268,7 +1268,9 @@ class Agent:
 
     async def __upload_answers_files(self):
         answers_file_dir_path = pathlib.Path(ktp_controller.files.ANSWERS_FILE_DIR)
-        for answers_file_path in answers_file_dir_path.glob("*/*.meb"):
+        for answers_file_path in sorted(
+            answers_file_dir_path.glob("*/*.meb"), key=lambda p: p.name
+        ):
             uploaded = await ktp_controller.agent.answers.upload_answers_file(
                 answers_file_path
             )
