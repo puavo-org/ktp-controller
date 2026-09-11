5c2f0a3552f61e20bd07a58bf944f5948219dc10
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2026-09-11 21:10:31 +0300

    redis: add simple CappedList

--- ktp_controller/redis.py
+++ ktp_controller/redis.py
@@ -47,3 +47,24 @@ async def pubsub(channel: str) -> typing.AsyncIterator[typing.Any]:
             yield pubsub_
         finally:
             await pubsub_.unsubscribe(channel)
+
+
+class CappedList:
+    def __init__(self, key: str, max_size: int, /) -> None:
+        if max_size < 0:
+            raise ValueError("max_size cannot be less than zero")
+        self.__key = f"ktp_controller:CappedList:{key}"
+        self.__max_size = max_size
+
+    async def lpush(self, data: typing.Any, /) -> None:
+        json_str = json.dumps(data, ensure_ascii=True)
+        async with redis.from_url("redis://127.0.0.1") as redis_client:
+            async with redis_client.pipeline() as pipeline:
+                pipeline.lpush(self.__key, json_str)
+                pipeline.ltrim(self.__key, 0, self.__max_size - 1)
+                await pipeline.execute()
+
+    async def getall(self, /) -> list[typing.Any]:
+        async with redis.from_url("redis://127.0.0.1") as redis_client:
+            json_strs = await redis_client.lrange(self.__key, 0, -1)
+            return [json.loads(s) for s in json_strs]
